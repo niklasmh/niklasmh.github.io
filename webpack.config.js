36dce93c32c1044c11cd3f37f00ba1a6d1926319
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-09-04 21:50:51 +0200

    Added react entry and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var path = require('path')
+var webpack = require('webpack')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  entry: './src/js/app.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract('style', 'css!less')
+      }
+    ]
+  },
+  lessLoader: {
+    includePath: [path.resolve(__dirname, './styles')]
+  },
+  plugins: [
+    /* Uncomment this when ready for prod
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    */
+    new ExtractTextPlugin('styles.css')
+  ]
+}
