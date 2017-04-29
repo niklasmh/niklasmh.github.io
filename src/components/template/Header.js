5a36c1224a72b4667e7c4a286ed1db581f23fa9a
niklasmh <nikasmh@hotmail.com>
2017-04-30 01:15:52 +0200

    Added initial page to build from

---
+++ src/components/template/Header.js
@@ -0,0 +1,13 @@
+import Component from 'inferno-component'
+
+class Header extends Component {
+  render () {
+    return (
+      <div className="head">
+        <h2>Niklas M. Hole</h2>
+      </div>
+    )
+  }
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'inferno'
+import App from './App'
+import './index.css'
 
-import debug from 'debug'
-const log = debug('app:log')
-
-if (ENV === 'production') {
-  debug.disable()
-} else {
-  debug.enable('*')
-  log('Logging is enabled!')
-
-  document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] +
-  ':35729/livereload.js?snipver=1"></' + 'script>')
-}
-
-ReactDOM.render(
-  <h1>Work!!!!</h1>,
-  document.querySelector('#app')
-)
+render(<App />, document.getElementById('app'))
