f25665e525ddbab06c8ae257a9836b32b7f4220b
niklasmh <nikasmh@hotmail.com>
2017-04-30 12:57:54 +0200

    Fixed header typography

--- src/components/template/Header.js
+++ src/components/template/Header.js
@@ -4,7 +4,11 @@ class Header extends Component {
   render () {
     return (
       <div className="head">
-        <h2><img src="/img/me.png" alt="Me" />Niklas M. Hole<span className="path">{this.props.path}</span></h2>
+        <h2>
+          <img src="/img/me.png" alt="Me" />
+          <span className="page-title">Niklas M. Hole</span>
+          <span className="path">{this.props.path}</span>
+        </h2>
       </div>
     )
   }
