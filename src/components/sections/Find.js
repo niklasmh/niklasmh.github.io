07b8c3c3d1e51cf3fa261c027fbd56d7cb374685
niklasmh <nikasmh@hotmail.com>
2017-06-10 19:57:32 +0200

    Added a find section

---
+++ src/components/sections/Find.js
@@ -0,0 +1,13 @@
+import Component from 'inferno-component'
+
+class Find extends Component {
+  render () {
+    return (
+      <section className="section">
+        Find me!
+      </section>
+    )
+  }
+}
+
+export default Find
