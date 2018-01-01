e37ee66f81ede3aad5514cded0baea4de30551d7
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-01-01 21:38:08 +0100

    Change find layout

--- src/components/sections/Find.js
+++ src/components/sections/Find.js
@@ -1,12 +1,13 @@
 import Component from 'inferno-component'
-import { Section } from '../layout'
+import { Section, SubSection } from '../layout'
 
 class Find extends Component {
   render() {
     return (
-      <Section className="find" title="Find me" {...this.props}>
-        <div className="sub-section">GitHub</div>
-        <div className="sub-section">Hi</div>
+      <Section row className="find" title="Find me" {...this.props}>
+        <SubSection>GitHub</SubSection>
+        <SubSection>Twitter</SubSection>
+        <SubSection>Facebook</SubSection>
       </Section>
     )
   }
