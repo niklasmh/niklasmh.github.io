4df912da3c6155c2fc0c9b5cba857fac2e472366
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-01-03 02:57:19 +0100

    Fetch repos and their commit stats using await

--- src/components/sections/Me.js
+++ src/components/sections/Me.js
@@ -2,8 +2,49 @@ import Component from 'inferno-component'
 import { Section, SubSection } from '../layout'
 
 class Me extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      repos: [],
+      repoData: {},
+    }
+
+    fetch('https://api.github.com/users/niklasmh/repos')
+    .then(res => res.json())
+    .then(repos => {
+      console.log(repos)
+      this.setState(Object.assign({}, this.state, { repos }))
+      this.fetchRepos(repos)
+    })
+
+  }
+
+  async fetchRepos(repos) {
+    try {
+      let repoData = {}
+      await Promise.all(repos.map(repo => {
+        return fetch(`https://api.github.com/repos/niklasmh/${repo.name}/stats/contributors`)
+        .then(res => res.json())
+        .then(stats => {
+          repoData[repo.name] = stats
+        })
+      }))
+
+      console.log(repoData)
+      this.setState(Object.assign({}, this.state, { repoData }))
+
+    } catch (err) {}
+  }
+
   render() {
     this.props.className = "me"
+
+    let repos = this.state.repos.map(repo => {
+      let stats = this.state.repoData[repo.name]
+
+      return <div>{repo.name} ({stats && stats[0].total})</div>
+    })
 
     return (
       <Section {...this.props}>
@@ -12,7 +53,9 @@ class Me extends Component {
             <h3>I am a front-end developer which have a passion for design in general.</h3>
             <p><b>Current favorites:</b> JavaScript, C++ and Docker.</p>
           </SubSection>
-          <SubSection flex="1 1 320px">Recent projects</SubSection>
+          <SubSection flex="1 1 320px"><b>Recent projects:</b>
+            {repos}
+          </SubSection>
         </SubSection>
         <SubSection>Recent commitment</SubSection>
       </Section>
