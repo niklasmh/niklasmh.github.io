d7fda842e8c3cf9bfd84769f0d04a6b7b855ccbe
niklasmh <nikasmh@hotmail.com>
2018-01-20 23:43:28 +0100

    Add api to make it possible to fetch all post names or a single post

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 var express = require('express')
 var app = express()
+var blog = require('./blog')
 
 app.use('/dist', express.static(__dirname + '/dist'))
 app.use('/img', express.static(__dirname + '/img'))
@@ -10,4 +11,21 @@ app.use(express.static(__dirname + '/build'))
 
 var listener = app.listen(process.env.PORT || 8080, function () {
   console.log('Listening on http://localhost:' + listener.address().port)
+})
+
+app.get('/api/v1/blog/:post?', function (req, res) {
+  var data = null
+
+  if (req.params.post) {
+    data = blog.getPost(req.params.post)
+  } else {
+    data = blog.getAllPosts()
+  }
+
+  if (data) {
+    res.setHeader('Content-Type', 'application/json; charset=utf-8')
+    res.send(JSON.stringify(data, null, 2))
+  } else {
+    res.statusCode(404)
+  }
 })
