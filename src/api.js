a71263c355c7bbbcc70ce3af7cb5973c9edc4df7
niklasmh <nikasmh@hotmail.com>
2018-01-21 22:15:06 +0100

    Make callback and catch optional in get request

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import { API_URL } from './constants'
 
-export const api = {
+export const API = {
   postRequest(url, req, callback, error) {
     req.headers = Object.assign({}, req.headers, {
       'Content-Type': 'application/json'
@@ -8,31 +8,34 @@ export const api = {
     req.method = 'POST'
     req.headers = new Headers(req.headers)
 
-    if (req.body) {
-      Object.assign(req.body, {
-        secret: SECRET
-      })
-    } else {
-      req.body = { secret: SECRET }
+    if (!req.body) {
+      req.body = {}
     }
 
     req.body = JSON.stringify(req.body)
 
     if (callback) {
-      return fetch(new Request(api.transformURL(url), req))
+      return fetch(new Request(API.transformURL(url), req))
       .then(res => res.json())
       .then(callback)
       .catch(error)
     } else {
-      return fetch(new Request(api.transformURL(url), req))
+      return fetch(new Request(API.transformURL(url), req))
     }
   },
 
   getRequest(url, callback, error) {
-    return fetch(api.transformURL(url))
-    .then(res => res.json())
-    .then(callback)
-    .catch(error)
+    let result = fetch(API.transformURL(url)).then(res => res.json())
+
+    if (callback) {
+      result = result.then(callback)
+    }
+
+    if (error) {
+      result = result.catch(error)
+    }
+
+    return result
   },
 
   joinPath(...paths) {
