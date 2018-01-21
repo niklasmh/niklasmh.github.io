a23e7bc78958d23c327e992ee6eb540829c28981
niklasmh <nikasmh@hotmail.com>
2018-01-21 21:39:30 +0100

    Add an api module

---
+++ src/api.js
@@ -0,0 +1,41 @@
+import { API_URL } from './constants'
+
+export const api = {
+  postRequest(url, req, callback, error) {
+    req.headers = Object.assign({}, req.headers, {
+      'Content-Type': 'application/json'
+    })
+    req.method = 'POST'
+    req.headers = new Headers(req.headers)
+
+    if (req.body) {
+      Object.assign(req.body, {
+        secret: SECRET
+      })
+    } else {
+      req.body = { secret: SECRET }
+    }
+
+    req.body = JSON.stringify(req.body)
+
+    if (callback) {
+      return fetch(new Request(API_URL + url, req))
+      .then(res => res.json())
+      .then(callback)
+      .catch(error)
+    } else {
+      return fetch(new Request(API_URL + url, req))
+    }
+  },
+
+  getRequest(url, callback, error) {
+    return fetch(API_URL + url)
+    .then(res => res.json())
+    .then(callback)
+    .catch(error)
+  },
+
+  joinPath(...paths) {
+    return paths.join('/').replace(/\/+/g, '/')
+  },
+}
