af15e1319c1cbf354d3845fe58f5a9c56776e238
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-01-22 04:01:00 +0100

    Use static api as default

--- server.js
+++ server.js
@@ -3,17 +3,15 @@ var express = require('express')
 var app = express()
 var blog = require('./blog')
 
-app.use('/dist', express.static(__dirname + '/dist'))
-app.use('/img', express.static(__dirname + '/img'))
-app.use('/static', express.static(__dirname + '/static'))
-app.use('/node_modules', express.static(__dirname + '/node_modules'))
 app.use(express.static(__dirname + '/build'))
+app.use('/api/v1/blog', express.static(__dirname + '/public/blog'))
 
 var listener = app.listen(process.env.PORT || 8080, function () {
   console.log('Listening on http://localhost:' + listener.address().port)
 })
 
-app.get('/api/v1/blog/:post?', function (req, res) {
+// Backup API if static API is not enough in the future
+app.get('/api/v0/blog/:post?', function (req, res) {
   var data = null
 
   if (req.params.post) {
