3241603ef258ea20770730daeced70bfacb6e62e
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-01-22 05:32:22 +0100

    Style blog to use article data

--- src/components/sections/Blog.js
+++ src/components/sections/Blog.js
@@ -1,15 +1,32 @@
 import Component from 'inferno-component'
 import { Section, SubSection } from '../layout'
+import { API } from '../../api'
 
 class Blog extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      articles: []
+    }
+  }
+
   render() {
     this.props.title = "Latest articles"
     this.props.name = "Blog"
     this.props.className = "blog"
 
+    let articles = this.state.articles.map(article => {
+      return (
+        <SubSection className="blog-article">
+          <h3>{article}</h3>
+        </SubSection>
+      )
+    })
+
     return (
-      <Section row subClassName="blog" {...this.props}>
-        Blog!
+      <Section nowrap subClassName="blog blog-article-list" {...this.props}>
+        {articles}
       </Section>
     )
   }
