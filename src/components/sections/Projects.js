a67a7175471be022a1a4c271d4ecf95e36293fbe
Niklas Molnes Hole <nikasmh@hotmail.com>
2019-01-01 23:05:51 +0100

    Add the knithub project

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -8,10 +8,20 @@ class Projects extends Component {
 
     let projects = [
       {
+        name: 'The Knithub',
+        tags: ['canvas', 'cra', 'now2.0'],
+        image: 'template-knithub.png',
+        description:
+          'Canvas GUI making knit and stitch patterns easier. Gift to girlfriend. This is a enhancement of the previous version.',
+        url: 'http://knithub.hanneko.no',
+        sourceCode: 'https://github.com/niklasmh/knithub',
+      },
+      {
         name: 'Flappy Ascend',
         tags: ['websocket', 'node', 'js'],
         image: 'template-flappy-small.png',
-        description: 'The Ascend NTNU version of flappy bird. Multiplayer over websockets. Used in the recruiting season for Ascend NTNU.',
+        description:
+          'The Ascend NTNU version of flappy bird. Multiplayer over websockets. Used in the recruiting season for Ascend NTNU.',
         url: 'http://flappy.ascendntnu.no/',
         sourceCode: 'https://github.com/AscendNTNU/flappy-ascend-game',
       },
@@ -19,7 +29,8 @@ class Projects extends Component {
         name: 'Ascend NTNU Website',
         tags: ['react', 'three', 'ts'],
         image: 'template-ascend-small.png',
-        description: 'Website for representing team Ascend NTNU in the IARC competition.',
+        description:
+          'Website for representing team Ascend NTNU in the IARC competition.',
         url: 'https://ascendntnu.no/',
         sourceCode: 'https://github.com/AscendNTNU/ascendntnu-web',
       },
@@ -27,7 +38,8 @@ class Projects extends Component {
         name: 'Bot Project',
         tags: ['vue', 'mysql', 'css'],
         image: '',
-        description: 'Teaching assistant bot for the program engineering course (TDT4140).',
+        description:
+          'Teaching assistant bot for the program engineering course (TDT4140).',
         url: 'http://tobot.niklasmh.no',
         sourceCode: 'https://github.com/niklasmh/tdt4140-bot-project',
       },
@@ -35,9 +47,11 @@ class Projects extends Component {
         name: 'Training Diary Project',
         tags: ['mysql', 'java', 'cli'],
         image: '',
-        description: 'A training diary using MySQL, Java and CLI. Made in TDT4145.',
+        description:
+          'A training diary using MySQL, Java and CLI. Made in TDT4145.',
         url: '',
-        sourceCode: 'https://github.com/niklasmh/tdt4145-training-diary-project',
+        sourceCode:
+          'https://github.com/niklasmh/tdt4145-training-diary-project',
       },
       {
         name: 'Web Dev Project',
@@ -75,7 +89,8 @@ class Projects extends Component {
         name: 'Lektordokka',
         tags: ['angular 1', 'yt', 'php'],
         image: 'template-lektordokka-small.png',
-        description: 'Site using YT videoes to explain physics. Currently down.',
+        description:
+          'Site using YT videoes to explain physics. Currently down.',
         url: 'http://lektordokka.no/',
         sourceCode: '',
       },
@@ -83,7 +98,8 @@ class Projects extends Component {
         name: 'Physics Engine',
         tags: ['game maker', 'css'],
         image: 'template-physics-engine-small.jpg',
-        description: 'Made a 2D physics engine from the bottom using math and physics from highschool.',
+        description:
+          'Made a 2D physics engine from the bottom using math and physics from highschool.',
         url: 'http://nikkapp.com/physics4.html',
         sourceCode: '',
       },
@@ -99,15 +115,18 @@ class Projects extends Component {
         name: 'Gravity Farm',
         tags: ['c#', 'physics', 'game'],
         image: 'template-gravity-farm-small.png',
-        description: 'My first game. Written in C# and released on windows phone with 96 tracks. Seems to be unavailable for the momemt.',
-        url: 'http://www.windowsphone.com/nb-no/store/app/gravity-farm/9ea88a34-0968-4702-9ef0-48c3de1d9c5f',
+        description:
+          'My first game. Written in C# and released on windows phone with 96 tracks. Seems to be unavailable for the momemt.',
+        url:
+          'http://www.windowsphone.com/nb-no/store/app/gravity-farm/9ea88a34-0968-4702-9ef0-48c3de1d9c5f',
         sourceCode: '',
       },
       {
         name: 'NikkApp',
         tags: ['php', 'css', 'html'],
         image: 'template-nikkapp-small.png',
-        description: 'Displaying apps and stuff. Learned a lot of CSS making it.',
+        description:
+          'Displaying apps and stuff. Learned a lot of CSS making it.',
         url: 'http://nikkapp.com/',
         sourceCode: '',
       },
@@ -119,7 +138,7 @@ class Projects extends Component {
     })
 
     this.state = {
-      projects
+      projects,
     }
 
     this.readMore = this.readMore.bind(this)
@@ -195,7 +214,7 @@ class Projects extends Component {
     if (!this.leftDown && this.leftDownTime < 300) {
       let left = document.querySelector('.project-list').scrollLeft
       let width = document.querySelector('.project-list > .project').offsetWidth
-      this.scrollDistAnimated(-width / 2 - (left + width / 2) % width, 0)
+      this.scrollDistAnimated(-width / 2 - ((left + width / 2) % width), 0)
     }
   }
 
@@ -203,7 +222,7 @@ class Projects extends Component {
     if (!this.rightDown && this.rightDownTime < 300) {
       let left = document.querySelector('.project-list').scrollLeft
       let width = document.querySelector('.project-list > .project').offsetWidth
-      this.scrollDistAnimated(width * 3 / 2 - (left + width / 2) % width, 0)
+      this.scrollDistAnimated((width * 3) / 2 - ((left + width / 2) % width), 0)
     }
   }
 
@@ -219,7 +238,7 @@ class Projects extends Component {
 
     if (time > 0) {
       this.interval = setInterval(() => {
-        scrollElement.scrollLeft += dist * intervals / time
+        scrollElement.scrollLeft += (dist * intervals) / time
       }, intervals)
 
       this.timeout = setTimeout(() => {
@@ -234,8 +253,8 @@ class Projects extends Component {
   }
 
   render() {
-    this.props.className = "projects"
-    this.props.title = "Projects"
+    this.props.className = 'projects'
+    this.props.title = 'Projects'
 
     let projects = this.state.projects.map((e, i) => {
       let tags = e.tags.map((tag, j) => {
@@ -243,7 +262,7 @@ class Projects extends Component {
       })
 
       let titleStyle = !!e.image && {
-        backgroundImage: `url(${process.env.PUBLIC_URL}/img/${e.image})`
+        backgroundImage: `url(${process.env.PUBLIC_URL}/img/${e.image})`,
       }
 
       let sliceTo = e.more ? -1 : e.description.indexOf('.', 110)
@@ -260,25 +279,30 @@ class Projects extends Component {
       return (
         <SubSection className="project">
           <SubSection style={titleStyle}>
-            <a href={e.url || e.sourceCode} target="_blank">{e.name}</a>
+            <a href={e.url || e.sourceCode} target="_blank">
+              {e.name}
+            </a>
           </SubSection>
           <SubSection>
             {tags}
-            {
-              !!e.sourceCode &&
-              <a href={e.sourceCode} className="tag code" title="Source code" target="_blank">
+            {!!e.sourceCode && (
+              <a
+                href={e.sourceCode}
+                className="tag code"
+                title="Source code"
+                target="_blank"
+              >
                 Code <FaIcon name="code" />
               </a>
-            }
+            )}
           </SubSection>
           <SubSection className="project-description" style={overflowStyle}>
             {description}
-            {
-              (e.more || sliceTo !== -1) &&
+            {(e.more || sliceTo !== -1) && (
               <div onClick={() => this.readMore(i)} className="load-more">
                 {!e.more ? 'Read more' : 'Collapse'}
               </div>
-            }
+            )}
           </SubSection>
         </SubSection>
       )
@@ -286,24 +310,28 @@ class Projects extends Component {
 
     return (
       <Section row subClassName="project-list" {...this.props}>
-        <Icon className="scroll-left"
+        <Icon
+          className="scroll-left"
           onClick={this.scrollLeft}
           onMouseDown={this.leftDownHandler}
           onTouchStart={this.leftDownHandler}
           onMouseUp={this.leftUpHandler}
           onTouchEnd={this.leftUpHandler}
-          onTouchCancel={this.leftUpHandler}>
+          onTouchCancel={this.leftUpHandler}
+        >
           keyboard_arrow_left
         </Icon>
         {projects}
-        <div className="spacing"></div>
-        <Icon className="scroll-right"
+        <div className="spacing" />
+        <Icon
+          className="scroll-right"
           onClick={this.scrollRight}
           onMouseDown={this.rightDownHandler}
           onTouchStart={this.rightDownHandler}
           onMouseUp={this.rightUpHandler}
           onTouchEnd={this.rightUpHandler}
-          onTouchCancel={this.rightUpHandler}>
+          onTouchCancel={this.rightUpHandler}
+        >
           keyboard_arrow_right
         </Icon>
       </Section>
